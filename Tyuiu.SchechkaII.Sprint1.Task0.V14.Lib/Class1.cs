c10bdca91d2adebb85ba46cb02211a4ac5ec5929
ivan.shechka38 <ivan.shechka38@gmail.com>
2026-03-25 16:15:17 +0500

    t

--- Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib/Class1.cs
+++ Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib/Class1.cs
@@ -1,4 +1,5 @@
-﻿namespace Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib;
+﻿
+namespace Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib;
 
 public class Class1 : ISprint1Task0V14
 {
@@ -6,6 +7,11 @@ public class Class1 : ISprint1Task0V14
     public static int Calculate()
     {
         return 2 * 3 * 3 + 7;
+    }
+
+    public double Calculate(double x, double a, double y)
+    {
+        throw new NotImplementedException();
     }
 }
 
