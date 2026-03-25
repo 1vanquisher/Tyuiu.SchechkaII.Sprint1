2fffe5bcdd55e7538f6ba4002c5077131e9f8f4f
ivan.shechka38 <ivan.shechka38@gmail.com>
2026-03-25 15:56:57 +0500

    Task1.V16

---
+++ Tyuiu.SchechkaII.Sprint1.Task1.V16.Lib/ISprint_Task_V16.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SchechkaII.Sprint1.Task1.V16.Lib
+{
+    public interface ISprint_Task_V16
+    {
+       
+        double Calculate(double x, double a, double y);
+    }
+}
