5595fccac87d45430a9f7e707e1fe875548bbee4
ivan.shechka38 <ivan.shechka38@gmail.com>
2026-03-25 15:58:04 +0500

    Task1.V16

--- Tyuiu.SchechkaII.Sprint1.Task1.V16.Lib/DataService.cs
+++ Tyuiu.SchechkaII.Sprint1.Task1.V16.Lib/DataService.cs
@@ -1,10 +1,10 @@
 ﻿namespace Tyuiu.SchechkaII.Sprint1.Task1.V16.Lib
 {
-    public class DataService
+    public class DataService : ISprint_Task_V16
     {
         public double Calculate(double x, double a, double y)
         {
-            
+            // x * 5 * a + 2 * y
             return x * 5 * a + 2 * y;
         }
     }
