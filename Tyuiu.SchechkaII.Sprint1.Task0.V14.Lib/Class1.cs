72922aa145819cb4b2f7ca532267de257b5b12d1
ivan.shechka38 <ivan.shechka38@gmail.com>
2026-03-25 16:14:13 +0500

    Task

--- Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib/Class1.cs
+++ Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib/Class1.cs
@@ -1,9 +1,11 @@
 ﻿namespace Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib;
 
-public class Class1
+public class Class1 : ISprint1Task0V14
 {
+
     public static int Calculate()
     {
         return 2 * 3 * 3 + 7;
     }
 }
+
--- Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib/ISprint1Task0V14.cs
+++ Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib/ISprint1Task0V14.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace Tyuiu.SchechkaII.Sprint1.Task0.V14.Lib
 {
-    public interface ISprint1Task1V16
+    public interface ISprint1Task0V14
     {
-
+        /// <summary>
+        /// Вычисляет значение выражения x*5*a + 2*y
+        /// </summary>
+        /// <param name="x">Первый множитель</param>
+        /// <param name="a">Второй множитель</param>
+        /// <param name="y">Слагаемое</param>
+        /// <returns>Результат вычисления</returns>
         double Calculate(double x, double a, double y);
     }
 }
